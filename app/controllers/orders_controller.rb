9651c396be60927ab8b1216dc0cdc18d32102a3e
Jenica de Leon <jendl1794@gmail.com>
2017-03-09 00:55:15 +0000

    update

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,18 @@
+class OrdersController < ApplicationController
+include CurrentCart
+before_action :set_cart, only: [:new, :create]
+# key new method os the ensure_cart_isnt_empty 
+before_action :ensure_cart_isnt_empty, only: :new
+before_action :set_order, only: [:show, :edit, :update, :destroy]
+
+# GET /orders
+#...
+
+ private
+ def ensure_cart_isnt_empty
+ if @cart.line_items.empty?
+ redirect_to store_index_url, notice: 'Your cart is empty'
+ end
+ end
+end
+
